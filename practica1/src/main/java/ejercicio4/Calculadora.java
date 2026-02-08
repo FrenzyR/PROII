2f8837c36790a70e1e780708832b7220018d9a32
Frenzy <donroland_vr@proton.me>
2026-02-08 14:04:25 +0100

    EJ4

--- practica1/src/main/java/ejercicio4/Calculadora.java
+++ practica1/src/main/java/ejercicio4/Calculadora.java
@@ -1,4 +1,24 @@
 package ejercicio4;
 
 public class Calculadora {
+
+    public int sumar(int i, int y) {
+        return i + y;
+    }
+
+    public int sumar(int i, int y, int z) {
+        return i + y + z;
+    }
+
+    public double sumar(double v, double w) {
+        return (v + w);
+    }
+
+    public int multiplicar(int i, int y) {
+        return i * y;
+    }
+
+    public double multiplicar(int i, double y) {
+        return (i * y);
+    }
 }
--- practica1/src/main/java/ejercicio4/CalculadoraTest.java
+++ practica1/src/main/java/ejercicio4/CalculadoraTest.java
@@ -1,4 +1,42 @@
 package ejercicio4;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculadoraTest {
+    private Calculadora calc = new Calculadora();
+    int resultado;
+    double resultadoDecimal;
+
+    @Test
+    void testSumar() {
+
+        this.resultado = this.calc.sumar(2, 3);
+
+        assertEquals(5, resultado);
+    }
+
+    @Test
+    void testSumarTresInt(){
+        this.resultado = this.calc.sumar(2,3,4);
+        assertEquals(9,resultado);
+    }
+
+    @Test
+    void testSumarDecimales(){
+        this.resultadoDecimal = this.calc.sumar(5.2, 3.6);
+        assertEquals(8.8, resultadoDecimal);
+    }
+
+    @Test
+    void testMultiplicacion(){
+        this.resultado = this.calc.multiplicar(2,5);
+        assertEquals(10, resultado);
+    }
+
+    @Test
+    void testMultiplicacionUnDecimal(){
+        this.resultadoDecimal = this.calc.multiplicar(2,5.0);
+        assertEquals(10.0, resultadoDecimal);
+    }
 }
